5b497abf4e1ecd4bf9b6d836db09043377e9abcc
Kelabr <kelabrantes.me@gmail.com>
2025-06-08 16:35:23 -0300

    created update router

--- src/api/endpoints/users.py
+++ src/api/endpoints/users.py
@@ -3,7 +3,7 @@ from fastapi.responses import JSONResponse
 from fastapi import status
 from ...db.connection import connection
 from ...db.schemas import CreateUser, DeleteUser
-from ...db.querys import createUsers, deleteUser
+from ...db.querys import createUsers, deleteUser, updateUser
 from ...auth.security import createAccessToken
 
 
@@ -40,3 +40,21 @@ def delete_user(emailUser:DeleteUser): # Provavelmente vai ocorrer alteração -
             status_code=status.HTTP_200_OK,
             content={'menssage': f'Usuário deletdo - email: {emailUser.email} '}
         )
+
+
+@router.put('/update')
+def update_user(dataUserUpdate:dict):
+    response = updateUser(coon, dataUserUpdate)
+
+    if response:
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={'menssage': f'Usuário Atualizado'}
+        )
+    else:
+        return JSONResponse(
+            status_code=status.HTTP_409_CONFLICT,
+            content={'menssage': 'Erro ao atualizar '}
+        )
+    
+    
--- src/db/querys.py
+++ src/db/querys.py
@@ -33,4 +33,28 @@ def deleteUser(coon, email):
         return None
 
 
+def updateUser(coon, data:dict):
+    try:
+        with coon.cursor() as cur:
+
+            cur.execute('SELECT id FROM users WHERE email=%s', (data['email'], ))
+            result = cur.fetchone()
+
+            if not result:
+                print('Usuário não encontrado')
+                return None
+            
+            iduser = result[0]
+
+            cur.execute('UPDATE users SET name = COALESCE(%s, name), email = COALESCE(%s, email), phone = COALESCE(%s, phone), password = COALESCE(%s, password), sex = COALESCE(%s, sex) WHERE id=%s', (data['name'], data['email'], data['phone'], data['password'], data['sex'], iduser))
+            coon.commit()
+            return True
+        
+    except Exception as e:
+        print(f'Erro: {e}')
+        coon.rollback()
+
+        return None
+    
+
     
